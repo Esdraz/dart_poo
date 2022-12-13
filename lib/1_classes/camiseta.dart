b717042cb98c11bffe8a495c3c816f4af0e4ee95
Francisco <fsousa.jpa@gmail.com>
2022-12-12 21:06:14 -0300

    add atributos e metodos na classe camiseta

--- lib/1_classes/camiseta.dart
+++ lib/1_classes/camiseta.dart
@@ -1,11 +1,28 @@
+// public
+// private - ex. String? _cor; (underline deixa o metodo privado)
+
 // caracteristicas
 // comportamentos
 
 class Camiseta {
-  // atributos
+  // Atributos de instancia
   String? tamanho;
-  String? cor;
+  String? _cor;
   String? marca;
+
+  // Atributo de classe
+  static const String nome = 'Camiseta';
+
+  // Metodo de classe
+  static String recuperarNome() => nome;
+
+  // metodos gets e sets de cor
+  String? get cor => _cor;
+  set cor(String? cor) {
+    if (cor == 'Verde') {
+      throw Exception('Nao pode ser verde');
+    }
+  }
 
   String tipoDeLavagem() {
     if (marca == 'Nike') {
